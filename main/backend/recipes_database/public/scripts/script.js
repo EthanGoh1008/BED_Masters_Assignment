ceae92572a93511c0fac1d28b1573e699f3fc251
EthanGoh1008 <S10258606@connect.np.edu.sg>
2024-06-30 20:10:20 +0800

    POST function for recipes done

--- main/backend/recipes_database/public/scripts/script.js
+++ main/backend/recipes_database/public/scripts/script.js
@@ -24,3 +24,37 @@ document.addEventListener("DOMContentLoaded", () => {
       console.error("Error fetching recipes:", error);
     });
 });
+
+function addRecipe() {
+  var form = document.getElementById("addrecipeform");
+  if (form.style.display === "none" || form.style.display === "") {
+    form.style.display = "block";
+  } else {
+    form.style.display = "none";
+  }
+}
+
+function addRecipeToDB(event) {
+  event.preventDefault(); // Prevent the form from submitting in the default way
+
+  // Get the input values
+  const recipeTitle = document.getElementById("recipetitle").value;
+  const imageUrl = document.getElementById("imageurl").value;
+
+  // Create the data object to send to the server
+  const data = {
+    title: recipeTitle,
+    image_url: imageUrl,
+  };
+
+  // Send a POST request to the server
+  fetch("/recipes", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  }).finally(() => {
+    location.reload(); // Refresh the page after submission
+  });
+}
