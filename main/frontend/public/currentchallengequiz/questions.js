bd8fbb873cdfc0944f9d868a26e258dbde155f2a
EthanGoh1008 <S10258606@connect.np.edu.sg>
2024-07-28 00:22:19 +0800

    quiz questions added and start on fixing backend

--- main/frontend/public/currentchallengequiz/questions.js
+++ main/frontend/public/currentchallengequiz/questions.js
@@ -2,70 +2,67 @@
 let questions = [
   {
     numb: 1,
-    question: "What does HTML stand for?",
-    answer: "Hyper Text Markup Language",
-    options: [
-      "Hyper Text Preprocessor",
-      "Hyper Text Markup Language",
-      "Hyper Text Multiple Language",
-      "Hyper Tool Multi Language",
-    ],
+    question: "Which fruit is known for having the highest vitamin C content?",
+    answer: "Kiwi",
+    options: ["Apple", "Kiwi", "Strawberry", "Orange"],
   },
   {
     numb: 2,
-    question: "What does CSS stand for?",
-    answer: "Cascading Style Sheet",
-    options: [
-      "Common Style Sheet",
-      "Colorful Style Sheet",
-      "Computer Style Sheet",
-      "Cascading Style Sheet",
-    ],
+    question: "What is the world's most popular fruit based on consumption?",
+    answer: "Tomato",
+    options: ["Banana", "Apple", "Mango", "Tomato"],
   },
   {
     numb: 3,
-    question: "What does PHP stand for?",
-    answer: "Hypertext Preprocessor",
-    options: [
-      "Hypertext Preprocessor",
-      "Hypertext Programming",
-      "Hypertext Preprogramming",
-      "Hometext Preprocessor",
-    ],
+    question: "Which fruit is also known as the 'king of fruits'?",
+    answer: "Durian",
+    options: ["Durian", "Mangosteen", "Pineapple", "Watermelon"],
   },
   {
     numb: 4,
-    question: "What does SQL stand for?",
-    answer: "Structured Query Language",
-    options: [
-      "Stylish Question Language",
-      "Stylesheet Query Language",
-      "Statement Question Language",
-      "Structured Query Language",
-    ],
+    question: "What fruit is used to make traditional guacamole?",
+    answer: "Avocado",
+    options: ["Tomato", "Mango", "Avocado", "Papaya"],
   },
   {
     numb: 5,
-    question: "What does XML stand for?",
-    answer: "eXtensible Markup Language",
-    options: [
-      "eXtensible Markup Language",
-      "eXecutable Multiple Language",
-      "eXTra Multi-Program Language",
-      "eXamine Multiple Language",
-    ],
+    question:
+      "Which fruit is known for its ability to ripen other fruits due to the release of ethylene gas?",
+    answer: "Banana",
+    options: ["Banana", "Apple", "Cherry", "Grape"],
   },
-  // you can uncomment the below codes and make duplicate as more as you want to add question
-  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
-  //   {
-  //   numb: 6,
-  //   question: "Your Question is Here",
-  //   answer: "Correct answer of the question is here",
-  //   options: [
-  //     "Option 1",
-  //     "option 2",
-  //     "option 3",
-  //     "option 4"
-  //   ]
-  // },
+  {
+    numb: 6,
+    question: "Which of the following fruits is a good source of potassium?",
+    answer: "Banana",
+    options: ["Grape", "Orange", "Blueberry", "Banana"],
+  },
+  {
+    numb: 7,
+    question:
+      "Which fruit has varieties known as 'Honeycrisp' and 'Granny Smith'?",
+    answer: "Apple",
+    options: ["Pear", "Apple", "Plum", "Cherry"],
+  },
+  {
+    numb: 8,
+    question:
+      "Which fruit is known as a 'superfood' for its high antioxidant content?",
+    answer: "Blueberry",
+    options: ["Apple", "Blueberry", "Orange", "Grape"],
+  },
+  {
+    numb: 9,
+    question:
+      "What fruit is typically used to make the Italian dessert tiramisu?",
+    answer: "None",
+    options: ["Raspberry", "Cherry", "Pear", "None"],
+  },
+  {
+    numb: 10,
+    question:
+      "Which fruit is known to contain the enzyme bromelain, which can break down proteins?",
+    answer: "Pineapple",
+    options: ["Papaya", "Pineapple", "Kiwi", "Starfruit"],
+  },
 ];
