071f04186ffe726caf2a8e3a1fb0cf4cecc5de66
Liew Zhan Yang <148023902+LiewZhanYang@users.noreply.github.com>
2024-07-28 23:50:44 +0800

    done credit.html

--- main/backend/models/modelForum.js
+++ main/backend/models/modelForum.js
@@ -8,37 +8,18 @@ class Forum {
       const result = await pool.request().query("SELECT * FROM Forums");
       return result.recordset;
     } catch (error) {
-<<<<<<< HEAD
-      console.error("Error retrieving forums:", error.message);
-=======
       console.error("Error getting forums:", error);
->>>>>>> e79db061e6686308e69106552aba2959b1de3262
       throw error;
     }
   }
 
-<<<<<<< HEAD
-  static async createForum(newForumData) {
-    try {
-      const { title, description, image_url } = newForumData;
-=======
   static async createForum({ title, description, image_url }) {
     try {
->>>>>>> e79db061e6686308e69106552aba2959b1de3262
       const pool = await poolPromise;
       const result = await pool
         .request()
         .input("title", sql.NVarChar, title)
         .input("description", sql.NVarChar, description)
-<<<<<<< HEAD
-        .input("image_url", sql.NVarChar, image_url) // Ensure image_url is provided
-        .query(
-          "INSERT INTO Forums (title, description, image_url) VALUES (@title, @description, @image_url); SELECT SCOPE_IDENTITY() AS id"
-        );
-      return result.recordset[0].id;
-    } catch (error) {
-      console.error("Error creating forum:", error.message);
-=======
         .input("image_url", sql.NVarChar, image_url)
         .query(
           "INSERT INTO Forums (title, description, image_url) OUTPUT INSERTED.id VALUES (@title, @description, @image_url)"
@@ -46,19 +27,12 @@ class Forum {
       return result.recordset[0].id;
     } catch (error) {
       console.error("Error creating forum:", error);
->>>>>>> e79db061e6686308e69106552aba2959b1de3262
       throw error;
     }
   }
 
-<<<<<<< HEAD
-  static async updateForum(forumId, updatedForumData) {
-    try {
-      const { title, description, image_url } = updatedForumData;
-=======
   static async updateForum(forumId, { title, description, image_url }) {
     try {
->>>>>>> e79db061e6686308e69106552aba2959b1de3262
       const pool = await poolPromise;
       const result = await pool
         .request()
@@ -71,11 +45,7 @@ class Forum {
         );
       return result.rowsAffected[0] > 0;
     } catch (error) {
-<<<<<<< HEAD
-      console.error("Error updating forum:", error.message);
-=======
       console.error("Error updating forum:", error);
->>>>>>> e79db061e6686308e69106552aba2959b1de3262
       throw error;
     }
   }
@@ -89,11 +59,7 @@ class Forum {
         .query("DELETE FROM Forums WHERE id = @id");
       return result.rowsAffected[0] > 0;
     } catch (error) {
-<<<<<<< HEAD
-      console.error("Error deleting forum:", error.message);
-=======
       console.error("Error deleting forum:", error);
->>>>>>> e79db061e6686308e69106552aba2959b1de3262
       throw error;
     }
   }
